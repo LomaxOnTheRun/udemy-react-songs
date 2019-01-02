5292e6ec576995354910921d5db2040aa4e41c69
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2019-01-02 17:23:53 +1100

    Add SOngList to App + bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SongList from "./SongList";
 
 const App = () => {
-  return <div>I'm the app!</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
